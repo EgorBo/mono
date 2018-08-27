c9bf239b09c8e30909755ef1532c3e214b4537d8
EgorBo <egorbo@gmail.com>
2018-08-27 13:13:43 +0300

    Initial Unix support

--- mcs/class/corlib/corefx/SR.missing.cs
+++ mcs/class/corlib/corefx/SR.missing.cs
@@ -3,4 +3,6 @@ partial class SR
 	public const string ArgumentOutOfRange_ConsoleKey = "Console key values must be between 0 and 255 inclusive.";
 	public const string Arg_InvalidComObjectException = "Attempt has been made to use a COM object that does not have a backing class factory.";
 	public const string Arg_MustBeNullTerminatedString = "The string must be null-terminated.";
+	public const string InvalidTimeZone_NJulianDayNotSupported = "Julian n day in POSIX strings is not supported.";
+	public const string InvalidTimeZone_InvalidJulianDay = "Invalid Julian day in POSIX strings.";
 }
